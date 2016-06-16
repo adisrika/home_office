3521532e90eb69c28a824c821521796fab3da103
Srikanth Adimulam <adi.sriku366@gmail.com>
2016-06-16 20:01:18 +0530

    Correcting clock.js

--- clock.js
+++ clock.js
@@ -6,26 +6,78 @@ var run = function() {
     today,
     hour,
     minute,
-    date;
+    date,
+    weekdays = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'],
+    weekday;
 
     date = new Date();
     hour = date.getHours();
     minute = date.getMinutes();
     today = date.getDate() + '-' + date.getMonth();
+    weekday = weekdays[date.getDay()];
 
-    if (hour >= 9 && hour < 14) {
+    if (hour >= 9 && hour < 16) {
         request('https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + home + '&destinations=' + office + '&departure_time=now&key=AIzaSyAqWgYh1ytIpb0njggpA4b3jdcBCtq5Uf8', function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 body = JSON.parse(body);
-                fs.appendFile(today + '-home-office.txt', hour + ':' + minute + ' ' + body.rows[0].elements[0].duration_in_traffic.value);
+                request({
+                    url: "https://time-in-traffic.firebaseio.com/home_office/" + today + ".json",
+                    method: 'POST',
+                    json: {
+                        hour: hour,
+                        minute: minute,
+                        time_in_traffic: body.rows[0].elements[0].duration_in_traffic.value
+                    }
+                }, function (error, response, body) {
+                    if (error) {
+                        console.log(error)
+                    }
+                });
+                request({
+                    url: "https://time-in-traffic.firebaseio.com/home_office/" + weekday + ".json",
+                    method: 'POST',
+                    json: {
+                        hour: hour,
+                        minute: minute,
+                        time_in_traffic: body.rows[0].elements[0].duration_in_traffic.value
+                    }
+                }, function (error, response, body) {
+                    if (error) {
+                        console.log(error)
+                    }
+                });
             }
         });
     } else if (hour >= 16 && hour < 21) {
         request('https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + office + '&destinations=' + home + '&departure_time=now&key=AIzaSyAqWgYh1ytIpb0njggpA4b3jdcBCtq5Uf8', function (error, response, body) {
             if (!error && response.statusCode == 200) {
                 body = JSON.parse(body);
-                console.log(hour + ':' + minute + ' ' + body.rows[0].elements[0].duration_in_traffic.value);
-                fs.appendFile(today + '-office-home.txt', hour + ':' + minute + ' ' + body.rows[0].elements[0].duration_in_traffic.value);
+                request({
+                    url: "https://time-in-traffic.firebaseio.com/office_home/" + today + ".json",
+                    method: 'POST',
+                    json: {
+                        hour: hour,
+                        minute: minute,
+                        time_in_traffic: body.rows[0].elements[0].duration_in_traffic.value
+                    }
+                }, function (error, response, body) {
+                    if (error) {
+                        console.log(error)
+                    }
+                });
+                request({
+                    url: "https://time-in-traffic.firebaseio.com/office_home/" + weekday + ".json",
+                    method: 'POST',
+                    json: {
+                        hour: hour,
+                        minute: minute,
+                        time_in_traffic: body.rows[0].elements[0].duration_in_traffic.value
+                    }
+                }, function (error, response, body) {
+                    if (error) {
+                        console.log(error)
+                    }
+                });
             }
         });
     }
